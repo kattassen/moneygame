0ee08fbbebd0c1aec397470c4871870a1c74a5bb
Simon Carlsson <simon@orval.(none)>
2011-05-19 21:32:55 +0200

    More work done, 3 strategys etc

--- moneyGame.java
+++ moneyGame.java
@@ -1,5 +1,7 @@
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 class moneyGame {
     
@@ -7,8 +9,10 @@ class moneyGame {
 
     private static int m_noOfPlayers;
     private static int m_noOfRounds;
+    private static int m_noOfCoins;
 
     public static void main(String[] args) {
+        moneyGamePlayer player;
         System.out.println("$$$$ Money Game $$$$"); 
 
         parseArgs(args);
@@ -16,33 +20,51 @@ class moneyGame {
         ArrayList playerList = new ArrayList();
 
         for (int i = 0; i < m_noOfPlayers; i++) {
-            moneyGamePlayer player = new moneyGamePlayer(50, "Player" + (i + 1));
+            player = new moneyGamePlayer(m_noOfCoins, i , m_noOfRounds ,"Player" + (i + 1));
             playerList.add(player);
         }
-            System.out.println("Size of list is " + playerList.size());
 
         for (int i = 0; i < m_noOfRounds; i++) {
             int leadingCoins = -1;
             int leadingPlayerIndex;
             int coins;
 
+            System.out.println("----------------------------------\n Round " + (i+1));
+
             for(int j = 0; j < m_noOfPlayers; j++) {
-                moneyGamePlayer player = (moneyGamePlayer)playerList.get(j);
+                player = (moneyGamePlayer)playerList.get(j);
 
                 coins = player.putCoins();
-/*                if (coins > leadingCoins) {
-                    leadingCoins = coins;
-                    leadingPlayerIndex
-                }*/
             }
 
+            Collections.sort(playerList, new Comparator(){
+                // Sort the list in descending order by how many coins put 
+                public int compare(Object o1, Object o2) {
+                   moneyGamePlayer p1 = (moneyGamePlayer) o1;
+                   moneyGamePlayer p2 = (moneyGamePlayer) o2;
+                   return p2.getCoinsThisRound() - p1.getCoinsThisRound();
+                }
+ 
+            });
+
+            for (int j = 0; j < m_noOfPlayers; j++) {
+                player = (moneyGamePlayer)playerList.get(j);
+                player.addPoints(m_noOfPlayers - j);
+            }
         }
 
-//        for(int i = 1; i < m_noOfPlayers; i++) {
-//            player.getCoinsRound();
-//        }
-    }
+        Collections.sort(playerList, new Comparator(){
+            // Sort the list in descending order by how many coins put 
+            public int compare(Object o1, Object o2) {
+               moneyGamePlayer p1 = (moneyGamePlayer) o1;
+               moneyGamePlayer p2 = (moneyGamePlayer) o2;
+               return p2.getCoinsThisRound() - p1.getCoinsThisRound();
+            }
+        }
 
+        player = (moneyGamePlayer)playerList.get(0);
+        System.out.println("----------------------------------\nWinner is: " + player.getName());
+    }
 
     private static boolean parseArgs(String[] args) {
         for(int i = 0; i < args.length; i++)
@@ -52,6 +74,7 @@ class moneyGame {
 
             m_noOfPlayers = Integer.parseInt(args[0]);
             m_noOfRounds  = Integer.parseInt(args[1]);
+            m_noOfCoins   = Integer.parseInt(args[2]);
         return true;
     }
 }
--- moneyGamePlayer.java
+++ moneyGamePlayer.java
@@ -1,11 +1,30 @@
+/* Strategys: 0 - Even 
+*             1 - Total Random
+*             2 - Max 20 Random
+*
+*/
+
+import java.util.Random;
 
 class moneyGamePlayer {
-    private int m_coinsStart, m_coinsLeft, m_coinsThisRound;
+    private int m_coinsStart     = 0; 
+    private int m_coinsLeft      = 0;
+    private int m_coinsThisRound = 0;
+    private int m_points         = 0;
+    private int m_strategy       = 0;
+    private int m_roundsLeft     = 0;
+
     private String m_name;
 
-    public moneyGamePlayer(int coins, String name) {
+    private Random rand;
+
+    public moneyGamePlayer(int coins, int strategy, int rounds, String name) {
         m_coinsStart = m_coinsLeft = coins;
         m_name = name;
+        m_roundsLeft = rounds;
+        m_strategy = strategy;
+
+        rand = new Random();
 
         if (moneyGame.m_verbose)
             System.out.println(m_name + " added");
@@ -15,14 +34,46 @@ class moneyGamePlayer {
     }
 
     public int putCoins() {
-        m_coinsThisRound = 4;
+        m_coinsThisRound = selectAmount();
         m_coinsLeft = m_coinsLeft - m_coinsThisRound;
+        m_roundsLeft--;
         if (moneyGame.m_verbose)
             System.out.println(m_name + " put " +  m_coinsThisRound);
         return m_coinsThisRound;
     }
 
-    public int getCoinsRound() {
+    public int getCoinsThisRound() {
         return m_coinsThisRound;
+    }
+
+    public String getName() {
+        return m_name;
+    }
+
+    public void addPoints(int points) {
+        m_points = m_points + points;
+        if (moneyGame.m_verbose)
+            System.out.println(m_name + " got " +  points + " points this round. Total: " + m_points);
+    }
+
+    private int selectAmount() {
+        int value;
+        if (m_roundsLeft == 0)
+            return m_coinsLeft; 
+        switch (m_strategy) {
+            case 0:
+                // Even distribution over rounds
+                return m_coinsLeft/m_roundsLeft;
+            case 1:
+                // Total randomness
+                return rand.nextInt(m_coinsLeft);
+            default:
+                // "Even" randomness
+                if (1 == rand.nextInt(1))
+                    return m_coinsLeft/m_roundsLeft + rand.nextInt(m_coinsLeft/m_roundsLeft);
+                else
+                    return m_coinsLeft/m_roundsLeft - rand.nextInt(m_coinsLeft/m_roundsLeft);
+                
+        }
     }
 }
