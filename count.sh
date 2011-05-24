bee5222a7d9e0e6d8f67332345d4e84f701605c0
Simon Carlsson <simon@orval.(none)>
2011-05-24 21:06:48 +0200

    Added scripts + minor changes

---
+++ count.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+cat result.txt |grep Player1 -c
+cat result.txt |grep Player2 -c
+cat result.txt |grep Player3 -c
+cat result.txt |grep Player4 -c
+cat result.txt |grep Player5 -c
+cat result.txt |grep Player6 -c
+cat result.txt |grep Player7 -c
+cat result.txt |grep Player8 -c
+cat result.txt |grep Player9 -c
---
+++ runme.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+i=10
+#for i in 1 2 3 4 5
+while [ 1 ]
+do
+#	echo "Hoola"
+	java moneyGame 9 9 100 >> result.txt
+done 
